1151f401d1981b196210e475e793587351413c43
Patrick <109246840+SotEEluna@users.noreply.github.com>
2026-02-02 16:29:59 +0100

    Update logging.psm1

--- scripts/module/logging.psm1
+++ scripts/module/logging.psm1
@@ -1,15 +1,37 @@
-# This Function Logs the events of the whole tool:
+# Create a Log File and start logging for the script:
+$path = "$(Split-Path (Split-Path $PSScriptRoot -Parent) -Parent)\logs\"
+$logfile = "$($path)$(Get-Date -Format 'd')_log.txt"
 
-$path = "$(Join-Path (Split-Path $PSScriptRoot -Parent)\logs\"
-$logfile = "$($path)\(Get-Date -format 'd')_log.txt"
-
-if (-not(Test-Path $logfile) {
-  New-Item -ItemType File -Force -Path $logfile | Out-Null
+if (-not (Test-Path $logfile)) {
+    New-Item -ItemType File -Path $logfile -Force | Out-Null
 }
+else {
+    Clear-Content -Path $logfile -Force
+}
+
 
 function Write-Log {
-  param (
-      $text
-  )
-  "$($text)" | Out-File $logfile -Append
+    param(
+        $text,
+        [ValidateSet('INFO','SYS','ERROR','SUCCESS')][string]$level = 'INFO'  
+    )
+    "[$(Get-Date -Format 'HH:mm:ss')][$($level)]: $($text)" | Out-File -FilePath $logfile -Append
+   
+    switch ($level) {
+        "INFO" {
+            Write-Host -ForegroundColor Yellow "[$($level)]: $($text)"
+        }
+        "ERROR" {
+            Write-Host -ForegroundColor Red "[$($level)]: $($text)"
+        }
+        "SYS" {
+            Write-Host -ForegroundColor Cyan "[$($level)]: $($text)"
+        }
+        "SUCCESS" {
+            Write-Host -ForegroundColor Green "[$($level)]: $($text)"
+        }
+        Default {"INFO"}
+    }
 }
+
+Export-ModuleMember -Function Write-Log
